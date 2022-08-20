aa7d58d2457cf592dfc994096fe544dc9fae4f21
Duncan Pierce <duncan@duncanpierce.org>
2022-08-20 20:33:52 +0100

    Correct (I hope) Go doc.

--- vec32/doc.go
+++ vec32/doc.go
@@ -1,15 +1,13 @@
-package vec32
-
 /*
 Package vec32 provides functions for efficiently manipulating fixed-length arrays of size 32.
 
 It is one of several packages, each operating on a single size of array:
 
-* [github.com/duncanpierce/vector/vec2] for arrays of length 2
-* [github.com/duncanpierce/vector/vec4] for arrays of length 4
-* [github.com/duncanpierce/vector/vec8] for arrays of length 8
-* [github.com/duncanpierce/vector/vec16] for arrays of length 16
-* [github.com/duncanpierce/vector/vec32] for arrays of length 32
-* [github.com/duncanpierce/vector/vec64] for arrays of length 64
-
+  - [github.com/duncanpierce/vector/vec2] for arrays of length 2
+  - [github.com/duncanpierce/vector/vec4] for arrays of length 4
+  - [github.com/duncanpierce/vector/vec8] for arrays of length 8
+  - [github.com/duncanpierce/vector/vec16] for arrays of length 16
+  - [github.com/duncanpierce/vector/vec32] for arrays of length 32
+  - [github.com/duncanpierce/vector/vec64] for arrays of length 64
 */
+package vec32
--- vec64/doc.go
+++ vec64/doc.go
@@ -1,15 +1,13 @@
-package vec64
-
 /*
 Package vec64 provides functions for efficiently manipulating fixed-length arrays of size 64.
 
 It is one of several packages, each operating on a single size of array:
 
-* [github.com/duncanpierce/vector/vec2] for arrays of length 2
-* [github.com/duncanpierce/vector/vec4] for arrays of length 4
-* [github.com/duncanpierce/vector/vec8] for arrays of length 8
-* [github.com/duncanpierce/vector/vec16] for arrays of length 16
-* [github.com/duncanpierce/vector/vec32] for arrays of length 32
-* [github.com/duncanpierce/vector/vec64] for arrays of length 64
-
+  - [github.com/duncanpierce/vector/vec2] for arrays of length 2
+  - [github.com/duncanpierce/vector/vec4] for arrays of length 4
+  - [github.com/duncanpierce/vector/vec8] for arrays of length 8
+  - [github.com/duncanpierce/vector/vec16] for arrays of length 16
+  - [github.com/duncanpierce/vector/vec32] for arrays of length 32
+  - [github.com/duncanpierce/vector/vec64] for arrays of length 64
 */
+package vec64
