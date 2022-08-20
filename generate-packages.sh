6b08595c5ac2e9954e943ea39f6334343ec1f326
Duncan Pierce <duncan@duncanpierce.org>
2022-08-20 20:12:43 +0100

    Switch to vec16 as the template package from which others are generated.

--- generate-packages.sh
+++ generate-packages.sh
@@ -3,11 +3,12 @@
 exclude=n.go
 templateSize=16
 for otherSize in 2 4 8 32 64; do
-  pkg=vec$otherSize
+  pkg="vec$otherSize"
+  template="vec$templateSize"
   find "$pkg" -type f -not -name "$exclude" -exec rm "{}" ";"
-  for file in $(basename -a $template/*); do
+  for file in $(basename -a "$template"/*); do
     if [ $file != $exclude ]; then
-      sed "s/package *vec$templateSize/package ${pkg}/" "vec$templateSize/$file" | sed "s/Package *vec$templateSize/Package ${pkg}/" > ${pkg}/$file
+      sed "s/package *$template/package ${pkg}/" "$template/$file" | sed "s/Package *$template/Package ${pkg}/" > ${pkg}/$file
     fi
   done
 done
---
+++ vec4/doc.go
@@ -0,0 +1,15 @@
+package vec4
+
+/*
+Package vec4 provides functions for efficiently manipulating fixed-length arrays.
+
+It is one of several packages, each operating on a single size of array:
+
+* [github.com/duncanpierce/vec2] for arrays of length 2
+* [github.com/duncanpierce/vec4] for arrays of length 4
+* [github.com/duncanpierce/vec8] for arrays of length 8
+* [github.com/duncanpierce/vec16] for arrays of length 16
+* [github.com/duncanpierce/vec32] for arrays of length 32
+* [github.com/duncanpierce/vec64] for arrays of length 64
+
+*/
