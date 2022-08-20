e0e43c056443f931e0acc32ba35d8032d95455c0
Duncan Pierce <duncan@duncanpierce.org>
2022-08-20 20:19:43 +0100

    Correct package names in docs.

--- vec8/doc.go
+++ vec8/doc.go
@@ -5,11 +5,11 @@ Package vec8 provides functions for efficiently manipulating fixed-length arrays
 
 It is one of several packages, each operating on a single size of array:
 
-* [github.com/duncanpierce/vec2] for arrays of length 2
-* [github.com/duncanpierce/vec4] for arrays of length 4
-* [github.com/duncanpierce/vec8] for arrays of length 8
-* [github.com/duncanpierce/vec16] for arrays of length 16
-* [github.com/duncanpierce/vec32] for arrays of length 32
-* [github.com/duncanpierce/vec64] for arrays of length 64
+* [github.com/duncanpierce/vector/vec2] for arrays of length 2
+* [github.com/duncanpierce/vector/vec4] for arrays of length 4
+* [github.com/duncanpierce/vector/vec8] for arrays of length 8
+* [github.com/duncanpierce/vector/vec16] for arrays of length 16
+* [github.com/duncanpierce/vector/vec32] for arrays of length 32
+* [github.com/duncanpierce/vector/vec64] for arrays of length 64
 
 */
