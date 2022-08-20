29fd09248fb66e76bb558287c1281f992b076058
Duncan Pierce <duncan@duncanpierce.org>
2022-08-20 19:46:20 +0100

    Implement IsDuplicate()

---
+++ vec32/bool.go
@@ -0,0 +1,89 @@
+package vec32
+
+type Bool struct {
+	v uint64
+}
+
+const (
+	allBits = (1 << N) - 1
+)
+
+func Full() (r Bool) {
+	return Bool{allBits}
+}
+
+func Single() (r Bool) {
+	return Bool{1}
+}
+
+func (m Bool) True(i int) bool {
+	if m.v&(1<<i) != 0 {
+		return true
+	}
+	return false
+}
+
+func (m Bool) Set(i int, b bool) (r Bool) {
+	r.v = m.v &^ (1 << i)
+	if b {
+		r.v |= 1 << i
+	}
+	return
+}
+
+func (m Bool) And(n Bool) (r Bool) {
+	r.v = m.v & n.v
+	return
+}
+
+func (m Bool) AndNot(n Bool) (r Bool) {
+	r.v = m.v &^ n.v
+	return
+}
+
+func (m Bool) Or(n Bool) (r Bool) {
+	r.v = m.v | n.v
+	return
+}
+
+func (m Bool) Xor(n Bool) (r Bool) {
+	r.v = m.v ^ n.v
+	return
+}
+
+func (m Bool) Not() (r Bool) {
+	r.v = m.v ^ allBits
+	return
+}
+
+func (m Bool) Any() bool {
+	return m.v != 0
+}
+
+func (m Bool) All() bool {
+	return m.v == allBits
+}
+
+func (m Bool) ShiftLeft(i int) (r Bool) {
+	r.v = m.v << i
+	return
+}
+
+func (m Bool) ShiftRight(i int) (r Bool) {
+	r.v = m.v >> i
+	return
+}
+
+func (m Bool) ForTrue(f func(i int)) {
+	for i := 0; i < N; i++ {
+		if m.True(i) {
+			f(i)
+		}
+	}
+}
+
+func (m Bool) For(f func(i int, c bool)) {
+	for i := 0; i < N; i++ {
+		f(i, m.True(i))
+	}
+}
---
+++ vec4/horizontal.go
@@ -0,0 +1,18 @@
+package vec4
+
+import "github.com/duncanpierce/vector/constraintsExt"
+
+/*
+IsDuplicate checks elements of vector a for values which are equal to one another. If several elements have the same value,
+exactly one is chosen as the original. Elements other than the original are deemed to be duplicates and have the corresponding bit set
+in the boolean vector result.
+It is not defined which value is chosen as the original. Your code should assume it is chosen at random.
+*/
+func IsDuplicate[T constraintsExt.Number](m Bool, a [N]T) (r Bool) {
+	seen := map[T]bool{}
+	m.ForTrue(func(i int) {
+		r.Set(i, seen[a[i]])
+		seen[a[i]] = true
+	})
+	return
+}
