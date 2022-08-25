f49df5fccd92d65f6d7c8a249951abd1f0c6e9d4
Duncan Pierce <duncan@duncanpierce.org>
2022-08-25 14:37:57 +0100

    Added conversion of numeric types.

---
+++ vec/convert.go
@@ -0,0 +1,14 @@
+package vec
+
+import "github.com/duncanpierce/vector/constraintsExt"
+
+// Convert returns a vector with all elements converted to another numeric type. Complex numbers cannot be converted to other types.
+func Convert[From, To constraintsExt.OrderedNumber, FromV constraintsExt.Vector[From], ToV constraintsExt.Vector[To]](z *ToV, x *FromV) {
+	lz := len(*z)
+	if lz != len(*x) {
+		panic("input and result vectors must be the same length")
+	}
+	for i := 0; i < lz; i++ {
+		(*z)[i] = To((*x)[i])
+	}
+}
