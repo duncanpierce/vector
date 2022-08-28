43dcead1180cfbf93bd9f3d543ff79201f18d58a
Duncan Pierce <duncan@duncanpierce.org>
2022-08-28 16:29:50 +0100

    Add rounding.

--- vec/round.go
+++ vec/round.go
@@ -2,7 +2,17 @@ package vec
 
 import (
 	"golang.org/x/exp/constraints"
+	"math"
 )
 
-func Round[E constraints.Float, V slicer[E]](z, x *V) {
+func Round[E constraints.Float, XZ Vector[E]](z, x XZ) {
+	unary[E, E](z, x, func(x E) E {
+		return E(math.Round(float64(x)))
+	})
+}
+
+func RoundToEven[E constraints.Float, XZ Vector[E]](z, x XZ) {
+	unary[E, E](z, x, func(x E) E {
+		return E(math.RoundToEven(float64(x)))
+	})
 }
