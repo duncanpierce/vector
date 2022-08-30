c833ea85a0d5495aabaa872d57d132800f40bb30
Duncan Pierce <duncan@duncanpierce.org>
2022-08-30 14:20:38 +0100

    Count, All, None, Any for Bool.

--- vec/bool.go
+++ vec/bool.go
@@ -7,3 +7,26 @@ func IsSet[X Bool](x X, lane int) bool {
 	}
 	return false
 }
+
+func Any[X Bool](x X) bool {
+	return Count(x) != 0
+}
+
+func All[X Bool](x X) bool {
+	return Count(x) == x.lanes().nLanes
+}
+
+func None[X Bool](x X) bool {
+	return Count(x) == 0
+}
+
+func Count[X Bool](x X) int {
+	l := x.lanes()
+	n := 0
+	for i := 0; i < l.nLanes; i++ {
+		if IsSet(x, i) {
+			n++
+		}
+	}
+	return n
+}
