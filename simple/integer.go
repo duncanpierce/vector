b32f4ed9ee1ba1599d9c90fdf0cb0a70cb4c3fce
Duncan Pierce <duncan@duncanpierce.org>
2022-09-05 16:15:13 +0100

    Add integer operations.

---
+++ simple/integer.go
@@ -0,0 +1,39 @@
+package simple
+
+import (
+	"github.com/duncanpierce/vector/constraintsExt"
+	"github.com/duncanpierce/vector/simple/lanes"
+	"golang.org/x/exp/constraints"
+)
+
+func And[E constraints.Integer, XYZ constraintsExt.Vector[E]](z *XYZ, m *lanes.Bool, x, y *XYZ) {
+	binary(z, m, x, y, func(x, y E) E {
+		return x & y
+	})
+}
+
+func AndNot[E constraints.Integer, XYZ constraintsExt.Vector[E]](z *XYZ, m *lanes.Bool, x, y *XYZ) {
+	binary(z, m, x, y, func(x, y E) E {
+		return x &^ y
+	})
+}
+
+func Or[E constraints.Integer, XYZ constraintsExt.Vector[E]](z *XYZ, m *lanes.Bool, x, y *XYZ) {
+	binary(z, m, x, y, func(x, y E) E {
+		return x | y
+	})
+}
+
+func Xor[E constraints.Integer, XYZ constraintsExt.Vector[E]](z *XYZ, m *lanes.Bool, x, y *XYZ) {
+	binary(z, m, x, y, func(x, y E) E {
+		return x ^ y
+	})
+}
+
+func Not[E constraints.Integer, XYZ constraintsExt.Vector[E]](z *XYZ, m *lanes.Bool, x, y *XYZ) {
+	var allBits E
+	allBits--
+	unary(z, m, x, func(x E) E {
+		return x ^ allBits
+	})
+}
