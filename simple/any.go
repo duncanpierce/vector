07d56d784f9e86547f43ef86a6cc94d50cc17abd
Duncan Pierce <duncan@duncanpierce.org>
2022-09-05 17:37:43 +0100

    Add Permute, Gather, Scatter, Address. WIP on Shuffle.

--- simple/any.go
+++ simple/any.go
@@ -3,6 +3,7 @@ package simple
 import (
 	"github.com/duncanpierce/vector/constraintsExt"
 	"github.com/duncanpierce/vector/simple/lanes"
+	"golang.org/x/exp/constraints"
 )
 
 func Copy[E any, XZ constraintsExt.Vector[E]](z *XZ, m *lanes.Bool, x *XZ) {
@@ -33,3 +34,56 @@ func Zero[E any, Z constraintsExt.Vector[E]](z *Z, m *lanes.Bool) {
 		(*z)[i] = zero
 	})
 }
+
+/*
+Permute sets vector z to elements from x selected using indices in y. An element from x may appear more than once in the result, or may be absent.
+*/
+func Permute[E any, I constraints.Integer, XZ constraintsExt.Vector[E], Y constraintsExt.Vector[I]](z *XZ, m *lanes.Bool, x *XZ, y *Y) {
+	lanes.RangeActive[E](z, m, func(i, j int) {
+		index := (*y)[i]
+		(*z)[i] = (*x)[index]
+	})
+}
+
+/*
+Gather sets elements of z to the values referenced by pointers in x.
+*/
+func Gather[E any, Z constraintsExt.Vector[E], X constraintsExt.Vector[*E]](z *Z, m *lanes.Bool, x *X) {
+	lanes.RangeActive[E](z, m, func(i, j int) {
+		(*z)[i] = *(*x)[i]
+	})
+}
+
+/*
+Address sets elements of vector z to the addresses of slice elements in x, indexed by y.
+*/
+func Address[E any, Z constraintsExt.Vector[*E], X constraintsExt.Vector[[]E], Y constraintsExt.Vector[int]](z *Z, m *lanes.Bool, x *X, y *Y) {
+	lanes.RangeActive[*E](z, m, func(i, j int) {
+		index := (*y)[i]
+		elem := (*x)[i][index]
+		(*z)[i] = &(elem)
+	})
+}
+
+/*
+Scatter sets values pointed to by elements of z to corresponding values in x.
+*/
+func Scatter[E any, Z constraintsExt.Vector[*E], X constraintsExt.Vector[E]](z *Z, m *lanes.Bool, x *X) {
+	lanes.RangeActive[*E](z, m, func(i, j int) {
+		*(*z)[i] = (*x)[i]
+	})
+}
+
+/*
+Shuffle exchanges w consecutive elements from x with their neighbouring elements and writes the result to z.
+When w == 0, Shuffle is equivalent to Copy. When w == 1, each element is exchanged with its neighbour.
+*/
+//func Shuffle[E any, XZ constraintsExt.Vector[E]](z *XZ, m *lanes.Bool, w int, x *XZ) {
+//	if w < 0 {
+//		panic("w cannot be less than 0")
+//	}
+//	temp := [64]E{}
+//	for _, xs := temp[:], (*x)[:]; len(xs) > 0; {
+//		//copy(ts, xs[:w])
+//	}
+//}
